0fb86a9e79db454c4a568f92987409d5cbdf1e68
pradeepsimba <96039278+pradeepsimba@users.noreply.github.com>
2024-07-31 01:49:06 +0530

    Update wednesday.py

--- newweek/wednesday.py
+++ newweek/wednesday.py
@@ -1251,6 +1251,125 @@ def common( db : Session , date : str , dataoptiopns   , option : str , statusda
             
             return commoncalculation(db,newquery, date)
         
+
+        elif option == "twenty"  and statusdata == 'Non-Charchable':
+            
+            list_ddata1 = set()
+
+           
+
+            db_res2 = db.query(models.TL).filter(
+                models.TL.type_of_activity == 'Non-Charchable',
+                
+                models.TL.status == 1,
+                models.TL.working_time.like(f"%{date}%")    
+            )
+
+            for row2 in db_res2:
+                list_ddata1.add(row2.Service_ID)
+
+            db_res2 = db.query(models.TL).filter(
+                models.TL.type_of_activity == 'Non-Charchable',
+                
+                models.TL.status == 1,
+                models.TL.Service_ID == models.HOLD.Service_ID,
+                or_(
+                    models.HOLD.hold_time_end.like(f"%{date}%"),
+                    models.TL.working_time.like(f"%{date}%")
+                )
+                
+            )
+
+            for row2 in db_res2:
+                list_ddata1.add(row2.Service_ID)
+            
+            db_res2 = db.query(models.TL).filter(
+                models.TL.type_of_activity == 'Non-Charchable',
+                
+                models.TL.status == 1,
+                models.TL.Service_ID == models.END_OF_DAY.Service_ID,
+                or_(
+                    models.END_OF_DAY.end_time_end.like(f"%{date}%"),
+                    models.TL.working_time.like(f"%{date}%")
+                )
+                
+            )
+
+            for row2 in db_res2:
+                list_ddata1.add(row2.Service_ID)
+                
+
+        # final query
+            newquery =  db.query(models.TL).filter(
+                models.TL.type_of_activity == 'Non-Charchable',
+                
+                models.TL.status == 1,
+                models.TL.Service_ID.in_(list_ddata1)
+            ).all()
+
+            
+            
+            return commoncalculation(db,newquery, date)
+        
+        elif option == "twenty"  and statusdata == 'CHARGABLE':
+            
+            list_ddata1 = set()
+
+           
+
+            db_res2 = db.query(models.TL).filter(
+                models.TL.type_of_activity == 'CHARGABLE',
+                
+                models.TL.status == 1,
+                models.TL.working_time.like(f"%{date}%")    
+            )
+
+            for row2 in db_res2:
+                list_ddata1.add(row2.Service_ID)
+
+            db_res2 = db.query(models.TL).filter(
+                models.TL.type_of_activity == 'CHARGABLE',
+                
+                models.TL.status == 1,
+                models.TL.Service_ID == models.HOLD.Service_ID,
+                or_(
+                    models.HOLD.hold_time_end.like(f"%{date}%"),
+                    models.TL.working_time.like(f"%{date}%")
+                )
+                
+            )
+
+            for row2 in db_res2:
+                list_ddata1.add(row2.Service_ID)
+            
+            db_res2 = db.query(models.TL).filter(
+                models.TL.type_of_activity == 'CHARGABLE',
+                
+                models.TL.status == 1,
+                models.TL.Service_ID == models.END_OF_DAY.Service_ID,
+                or_(
+                    models.END_OF_DAY.end_time_end.like(f"%{date}%"),
+                    models.TL.working_time.like(f"%{date}%")
+                )
+                
+            )
+
+            for row2 in db_res2:
+                list_ddata1.add(row2.Service_ID)
+                
+
+        # final query
+            newquery =  db.query(models.TL).filter(
+                models.TL.type_of_activity == 'CHARGABLE',
+                
+                models.TL.status == 1,
+                models.TL.Service_ID.in_(list_ddata1)
+            ).all()
+
+            
+            
+            return commoncalculation(db,newquery, date)
+        
 def user_wise_report(db: Session,date: str,option: str):
     list_data = []
     list_ddata1 = set()
